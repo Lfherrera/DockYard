18563fa2df8ed34643c1109e6b5ca5e0150d7a4c
Luis Herrera <donluisherrera>
2023-06-27 05:22:58 -0700

    finish Games: Guessing Game exercise

--- games/lib/guessinggame.ex
+++ games/lib/guessinggame.ex
@@ -2,7 +2,7 @@ defmodule Games.GuessingGame do
   def play() do
     guess = IO.gets("Guess a number between 1 and 10:\ ")
     guess = String.replace(guess,~r/\r|\n/, "")
-    number_generator = Enum.random(1..3)
+    number_generator = Enum.random(1..10)
     IO.inspect("#{number_generator}", label: "Random number")
     IO.inspect("#{guess}", label: "guess number")
         if guess == number_generator, do: "You win!", else: "Incorrect!"
