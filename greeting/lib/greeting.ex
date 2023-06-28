5cafb3c8154079ced1a15a364f5ea400a2dfce19
Luis Herrera <donluisherrera>
2023-06-28 10:27:44 -0700

    finish Executables reading

--- greeting/lib/greeting.ex
+++ greeting/lib/greeting.ex
@@ -1,8 +1,27 @@
 defmodule Greeting do
-  def main(args) do
-    {opts, _word, _error} = OptionParser.parse(args, switches: [time: :string])
-    IO.puts("Good #{opts[:time] || "morning"}!")
-    {opts, word, _error} = OptionParser.parse(switches: [upcase: :boolean])
-    # IO.puts("#{opts[:upcase], do: String.upcase(), Good morning"}!")
+  def main(args \\ []) do
+    args
+    |> parse_arg()
+    |> response()
+    |> IO.puts()
+
+    # {opts, _word, _error} = OptionParser.parse(args, switches: [time: :string])
+    # IO.puts("Good #{opts[:time] || "morning"}!")
+  end
+
+  defp parse_arg(args) do
+    {opts, word, _} =
+      args
+      |> OptionParser.parse(switches: [upcase: :boolean])
+
+    {opts, List.to_string(word)}
+  end
+
+  defp response({opts, word}) do
+    if opts[:upcase] do
+      String.upcase(word)
+    else
+      word
+    end
   end
 end
